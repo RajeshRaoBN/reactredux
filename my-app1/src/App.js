9f5fb16d9d82b9aa1b840f2c42193f19e865498e
Rajesh Narayana Rao <mailrbn@gmail.com>
2025-03-21 11:18:34 +1100

    initial commit

---
+++ my-app1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import mybutton from './components/mybutton';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Hello World
+        </p>
+      <mybutton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app1/src/components/mybutton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class mybutton extends Component {
+  render() {
+    return (
+      <div>
+        <button>mybutton</button>
+      </div>
+    )
+  }
+}
+
+export default mybutton
